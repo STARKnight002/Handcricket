4c871fb6776ab1724311326cf42cdba4e2d99e38
STARKnight002 <108864470+STARKnight002@users.noreply.github.com>
2022-07-11 12:17:37 +0530

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -1,47 +1,70 @@
 print('HANGMAN')
 print('''INSTRUCTIONS-
-YOU HAVE TO GUESS THE WORD WITHIN 5 TRIES\n
-Type a SPACE and ENTER in your zeroth guess to identify spaces in the given puzzle
-PLAY FAIR''')
+1)Player 1 gives the word to guess
+1)Player 2 has to guess the word within 5 tries
+\nPLAY FAIR''')
 def cls():
     print('\n'*1000)
-word1=input('Enter word to Guess - ')
-word=list(word1)
-cls()
-display=[]
-display.extend(word)
-n=len(word)
-for a in range(n):
-    display[a]='_'
-print(' '.join(display))
-count=0
-x=6
-while (count<n):
-    guess=input('GUESS A LETTER-')
-    if guess in word:
-        if guess in display:
-            print('Word already entered')
-        else:
-            if len(guess)==1:
-                for i in range(n):
-                    if word[i]==guess:
-                        display[i]=guess
-                        count+=1
-                print(' '.join(display))
-                if display==word:
-                    print('WAY TO GO!!!!')
-            else:
-                print('Enter single letter')
-    else:
-        if x<1:
-            print('Sorry')
-            print('THE WORD IS-',word1)
-            break
-        print('Letter Not Found. Try Again')
-        x=x-1
-        print("You have",x+1,"chances left")
-        
+    
+def enclosure():
+    global word1,word,display,n
+    word1=input('Enter the word/sentence to be found-')
+    word=list(word1)
+    cls()
+    display=[]
+    display.extend(word)
+    n=len(word)
+    for a in range(n):
+        display[a]='_'
 
+def guess():
+    global word1,word,display,n
+    count=0
+    x=5
+    blank=' '
+    while (count<n):
+        if (count==0) and (blank in word1):
+            guess=blank
+        else:
+            print(' '.join(display))
+            guess=input('GUESS A LETTER-')
+        if guess in word:
+            if guess in display:
+                print('Word already entered')
+            else:
+                if len(guess)==1:
+                    for i in range(n):
+                        if word[i]==guess:
+                            display[i]=guess
+                            count+=1
+                    if display==word:
+                        print(' '.join(display))
+                        print('WAY TO GO!!!!')
+                else:
+                    print('Enter single letter')
+        else:
+            if x<1:
+                print('Sorry')
+                print('THE WORD IS-',word1)
+                break
+            print('Letter Not Found. Try Again')
+            x=x-1
+            print("You have",x+1,"chances left")
+        
+if __name__=="__main__":
+    enclosure()
+    guess()
+    while True:
+        ask = input('Do you want to play again? (y/n) - ')
+        if ask=='y':
+            enclosure()
+            guess()
+        else:
+            print("Game Over")
+            break
+    
+        
+        
 
     
         
